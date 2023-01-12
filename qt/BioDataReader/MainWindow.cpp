8488db35604f65e66a07d255e1b518ccf1d84dbb
Dmitry Konyshev <d.konyshev@neurobotics.ru>
2023-01-12 10:11:33 +0300

    Add scroll

--- qt/BioDataReader/MainWindow.cpp
+++ qt/BioDataReader/MainWindow.cpp
@@ -3,7 +3,7 @@
 #include <QPushButton>
 #include <QFileDialog>
 #include <QLabel>
-
+#include <QElapsedTimer>
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
 {
@@ -12,7 +12,7 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
     auto grid = new QGridLayout(central);
     grid->setContentsMargins(0,0,0,0);
     setCentralWidget(central);
-    setMinimumSize(800, 600);
+    resize(QSize(800, 600));
 
     m_chartView = new QChartView();
 
@@ -37,24 +37,31 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
             labelFileName->setToolTip(fileInfo.absoluteFilePath());
 
             m_chartView->chart()->removeAllSeries();
+            m_signals.clear();
+
+            m_channelAmount = m_reader->channelAmount();
+            m_samplingRate = m_reader->samplingRate();
+            m_lengthSeconds = m_reader->lengthSamples() / m_samplingRate;
+            m_sampleSizeSeconds = 1.0 / m_samplingRate;
 
             int n = m_reader->lengthSamples();
-            int channelAmount = m_reader->channelAmount();
-            float sampleSize = 1000.0/m_reader->samplingRate();
-            for (int i = 0; i<channelAmount; i++)
+
+            for (int i = 0; i<m_channelAmount; i++)
             {
                 QString ch = m_reader->getChannelName(i);
                 QLineSeries *series = new QLineSeries();
+
                 series->setName(ch);
-                qDebug() << "CH" <<ch;
 
                 QVector<float> data = m_reader->getChannelDataFloat(i, 0, n);
                 float x = 0;
-                for (int j = 0; j<n; j++)
+                for (int j = 0; j < n; j++)
                 {
-                    series->append(QPointF(x, data[j]));
-                    x += sampleSize;
+                    series->append(x, data[j]);
+                    x += m_sampleSizeSeconds;
                 }
+
+                m_signals << series;
                 m_chartView->chart()->addSeries(series);
             }
         }
@@ -65,6 +72,7 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
         }
 
         m_chartView->chart()->createDefaultAxes();
+        rebuildScroll();
     });
 
     auto topLayout = new QHBoxLayout();
@@ -72,15 +80,37 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
     topLayout->addWidget(labelFileName, 100);
     topLayout->setContentsMargins(8,8,8,0);
 
+    m_scrollBar = new QScrollBar(Qt::Horizontal);
+    m_scrollBar->setRange(0, 0);
+    connect(m_scrollBar, &QScrollBar::valueChanged, [=](int value) {
+       m_posSeconds = value;
+       showData();
+    });
+
     grid->addLayout(topLayout, 0, 0);
-    grid->setRowStretch(1, 100);
-
-//    m_chartView->chart()->addSeries(m_series);
-
     grid->addWidget(m_chartView, 1, 0);
+    grid->addWidget(m_scrollBar, 2, 0);
+    grid->setRowStretch(1, 100);
 }
 
 MainWindow::~MainWindow()
 {
+}
+
+void MainWindow::rebuildScroll()
+{
+    m_visibleSamples = m_visibleSeconds * m_samplingRate;
+
+    m_scrollBar->blockSignals(true);
+    m_scrollBar->setRange(0, m_lengthSeconds - m_visibleSeconds);
+    m_scrollBar->blockSignals(false);
+
+    showData();
+}
+
+void MainWindow::showData()
+{
+    if (!m_chartView || !m_reader || m_lengthSeconds == 0 || m_channelAmount == 0 || m_samplingRate == 0) return;
+    m_chartView->chart()->axes(Qt::Horizontal).first()->setRange(m_posSeconds, m_posSeconds + m_visibleSeconds);
 }
 
--- qt/BioDataReader/MainWindow.h
+++ qt/BioDataReader/MainWindow.h
@@ -6,6 +6,7 @@
 #include <QLineSeries>
 #include <QChartView>
 #include "EDF/EDFReader.h"
+#include <QScrollBar>
 
 class MainWindow : public QMainWindow
 {
@@ -20,5 +21,20 @@ protected:
     QString m_path = "";
     QChartView *m_chartView = nullptr;
     EDFReader *m_reader = nullptr;
+    QScrollBar *m_scrollBar = nullptr;
+
+    QList<QLineSeries*> m_signals;
+
+    int m_posSeconds = 0;
+    int m_lengthSeconds = 0;
+    int m_visibleSeconds = 5;
+    int m_visibleSamples = 0;
+
+    int m_channelAmount = 0;
+    float m_samplingRate = 0;
+    float m_sampleSizeSeconds = 0;
+
+    void rebuildScroll();
+    void showData();
 };
 #endif // MAINWINDOW_H
