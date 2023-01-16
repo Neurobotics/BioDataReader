e462050ac5265f1277489328efdecde5146c0a79
Dmitry Konyshev <d.konyshev@neurobotics.ru>
2023-01-16 16:36:28 +0300

    py: Refactoring and comments

--- python/mainWindow.py
+++ python/mainWindow.py
@@ -1,11 +1,7 @@
-from PyQt5 import QtCore, QtWidgets, QtGui
-from PyQt5.QtWidgets import QMessageBox
+from PyQt5 import QtCore, QtWidgets
 from PyQt5.QtCore import pyqtSlot
 import pyqtgraph as pg
-from pyqtgraph import PlotWidget, plot
 from pyedflib import highlevel
-from scipy import signal
-from scipy.fft import fftshift
 import numpy as np
 from scipy.fft import fft, fftfreq
 
@@ -71,6 +67,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self.showMaximized()   
 
     def updateXrange(self):
+        # Выставление масштаба по горизонтали
         x = int(self.comboXrange.currentData())
         scrollableValue = int(self.duration - x)
         if self.duration > 0:
@@ -80,12 +77,14 @@ class MainWindow(QtWidgets.QMainWindow):
         self.updateXpos()
 
     def updateXpos(self):
+        # Обновление позиции просмотра
         x = int(self.comboXrange.currentData())
         start = self.scrollBar.value()
         end = start + x
         for plot in self.graphWidgets:
             plot.setXRange(start, end)
 
+        # Вычисление спектра текущих обозреваемых сигналов 
         sampleStart = int(start * self.samplingRate)
         sampleEnd = int(end * self.samplingRate)
 
@@ -95,55 +94,63 @@ class MainWindow(QtWidgets.QMainWindow):
         maxY = 1
         for ch in range(self.channelCount):
             sig = self.signals[ch]
+            # Выделение видимой части сигнала
             portion = []
             for i in range(sampleStart, sampleEnd):
                 portion.append(sig[i])          
 
+            # Быстрое преобразование Фурье
             yf = fft(portion)
+
+            # Переход от комплексных чисел
             yf2 = 2.0/N * np.abs(yf[0:N//2])
             
+            # Вычисление максимальной амплитуды
             maxSpectrum = max(yf2)
             if maxSpectrum > maxY: maxY = maxSpectrum
 
+            # Отрисовка
             self.spectrumWidgets[ch].clear()
             self.spectrumWidgets[ch].plot(xf, yf2)
         
+        # Выставление графического максимума
         for ch in range(self.channelCount):
             self.spectrumWidgets[ch].setYRange(0, maxY)
-
-            
+          
 
     def updateYrange(self):
+        # Обновление графических пределов графиков сигналов
         y = self.comboYrange.currentData()
         for plot in self.graphWidgets:
             plot.setYRange(-y, y)
 
-    @pyqtSlot()
     def openfile(self):
         fileName = QtWidgets.QFileDialog.getOpenFileName(self, "Open EDF", "", "EDF files (*.edf)")[0]
         if len(fileName) == 0: return
 
         self.path = fileName
 
+        # Очистка старых виджетов
         for widgetRow in self.graphWidgetRows:
             widgetRow.setParent(None)
         self.graphWidgetRows.clear()
         self.spectrumWidgets.clear()
         self.graphWidgets.clear()
 
-        fileInfo = QtCore.QFileInfo(fileName)
-        self.labelFileName.setToolTip(fileName)
-
+        # Чтение данных из заголовка записи
         self.signals, signal_headers, header = highlevel.read_edf(fileName)
         self.samplingRate = signal_headers[0]['sample_rate']
         self.channelCount = len(self.signals)
         self.durationSamples = len(self.signals[0])
         self.duration = self.durationSamples / self.samplingRate
 
+        fileInfo = QtCore.QFileInfo(fileName)
+        self.labelFileName.setToolTip(fileName)
         self.labelFileName.setText(fileInfo.fileName() + " (" + str(self.duration) + "s, " + str(self.samplingRate) + "Hz, " + str(self.channelCount) + "ch)")
 
         self.xStep = 1.0 / self.samplingRate
         for ch in range(self.channelCount):
+            # Создания набора (ряда) виджетов для каждого канала
             channelName = signal_headers[ch]['label']
             label = QtWidgets.QLabel(channelName)
             label.setMinimumWidth(32)
@@ -173,6 +180,7 @@ class MainWindow(QtWidgets.QMainWindow):
 
             self.graphWidgetRows.append(widgetRow)
             self.graphWidgetsLayout.addWidget(widgetRow)           
-
+        
+        # Обновление графических пределов 
         self.updateYrange()
         self.updateXrange()
