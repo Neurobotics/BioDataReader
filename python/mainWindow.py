4e3d1c8a087a108c23b969428f833a4bfbeb5f79
Dmitry Konyshev <d.konyshev@neurobotics.ru>
2023-01-16 13:21:09 +0300

    py: Add vertical scale, minify layouts

--- python/mainWindow.py
+++ python/mainWindow.py
@@ -9,13 +9,11 @@ class MainWindow(QtWidgets.QMainWindow):
     samplingRate = 0
     duration = 0
     path = ''
+    channelCount = 0
 
     def __init__(self, *args: object, **kwargs: object) -> object:
         super(MainWindow, self).__init__(*args, **kwargs)
         self.setupUi()
-
-    def closeEvent(self, event):
-        print("Close")
 
     def setupUi(self):
         # Создание управляющих элементов в верхнем ряду
@@ -24,15 +22,32 @@ class MainWindow(QtWidgets.QMainWindow):
         btnOpen = QtWidgets.QPushButton("Open")
         btnOpen.clicked.connect(lambda: self.openfile())
 
+        xRange = [ 1, 2, 4, 5]
+        self.comboXrange = QtWidgets.QComboBox()
+        for x in xRange:
+            self.comboXrange.addItem(str(x) + " s", x)
+
+        yRange = [ 1, 10, 20, 25, 50, 100, 200, 500, 1000, 5000, 10000 ]
+        self.comboYrange = QtWidgets.QComboBox()
+        for y in yRange:
+            self.comboYrange.addItem("±" + str(y) + " uV", y)
+        self.comboYrange.setCurrentIndex(yRange.index(100))            
+        self.comboYrange.currentIndexChanged.connect(self.updateYrange)
+
         buttonsLayout = QtWidgets.QHBoxLayout()
+        buttonsLayout.setContentsMargins(0, 0, 0, 0)
         buttonsLayout.addWidget(btnOpen, 0)
         buttonsLayout.addWidget(self.labelFileName, 100)
+        # buttonsLayout.addWidget(self.comboXrange, 0)
+        buttonsLayout.addWidget(self.comboYrange, 0)
 
         # Создание компоновки графиков
         self.graphWidgets = []
         self.graphWidgetRows = []
         graphWidgetsHolder = QtWidgets.QWidget()
+        graphWidgetsHolder.setContentsMargins(0, 0, 0, 0)
         self.graphWidgetsLayout = QtWidgets.QVBoxLayout(graphWidgetsHolder)
+        self.graphWidgetsLayout.setContentsMargins(0, 0, 0, 0)
 
         # Создание центрального виджета и самого окна          
         centralWidget = QtWidgets.QWidget()
@@ -42,6 +57,11 @@ class MainWindow(QtWidgets.QMainWindow):
         self.setWindowTitle("BioDataReader")  
         self.setCentralWidget(centralWidget)               
         self.showMaximized()            
+
+    def updateYrange(self):
+        y = self.comboYrange.currentData()
+        for plot in self.graphWidgets:
+            plot.setYRange(-y, y)
 
     @pyqtSlot()
     def openfile(self):
@@ -56,41 +76,41 @@ class MainWindow(QtWidgets.QMainWindow):
 
         fileInfo = QtCore.QFileInfo(fileName)
         self.labelFileName.setToolTip(fileName)
-        self.labelFileName.setText(fileInfo.fileName())
 
         signals, signal_headers, header = highlevel.read_edf(fileName)
         self.samplingRate = signal_headers[0]['sample_rate']
+        self.channelCount = len(signals)
         self.durationSamples = len(signals[0])
         self.duration = self.durationSamples / self.samplingRate
-        print(self.duration)
-        print(self.durationSamples)
-        print(self.samplingRate)
-        count = 0
-        for header in signal_headers:
-            ch = header['label']
-            label = QtWidgets.QLabel(ch)
+
+        self.labelFileName.setText(fileInfo.fileName() + " (" + str(self.duration) + "s, " + str(self.samplingRate) + "Hz, " + str(self.channelCount) + "ch)")
+
+        self.xStep = 1.0 / self.samplingRate
+        for ch in range(self.channelCount):
+            channelName = signal_headers[ch]['label']
+            label = QtWidgets.QLabel(channelName)
             label.setMinimumWidth(32)
             plot = pg.PlotWidget()
-            plot.setYRange(-100.0, 100.0)
 
             widgetRow = QtWidgets.QWidget()
             widgetRowLayout = QtWidgets.QHBoxLayout(widgetRow)
+            widgetRowLayout.setContentsMargins(0, 0, 0, 0)
             widgetRowLayout.addWidget(label)
             widgetRowLayout.addWidget(plot, 100)
             
             axisX = []
             axisY = []
             x = 0.0
-            xStep = 1.0 / self.samplingRate
-            signal = signals[count] 
+            
+            signal = signals[ch] 
             for i in range(self.durationSamples):
                 axisX.append(x)
-                x += xStep
+                x += self.xStep
                 axisY.append(signal[i])
 
             plot.plot(axisX, axisY)
+            self.graphWidgets.append(plot)
 
             self.graphWidgetRows.append(widgetRow)
             self.graphWidgetsLayout.addWidget(widgetRow)
-            
-            count += 1
+        self.updateYrange()
